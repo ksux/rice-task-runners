a6194f4868b54775221f3d8ecd95cc4b9bb1cb18
Chris Basham <csbasham@iu.edu>
2014-08-06 11:43:46 -0400

    Initial push of gulp tasks.

---
+++ gulp/styles.js
@@ -0,0 +1,23 @@
+var gulp = require('gulp');
+var less = require('gulp-less');
+var prefix = require('gulp-autoprefixer');
+var concat = require('gulp-concat');
+var browserSync = require('browser-sync');
+
+gulp.task('styles', function() {
+  return gulp.src([
+      './node_modules/normalize.css/normalize.css',
+      './src/assets/bower_components/highlight/src/styles/github.css',
+      './src/assets/styles/main.less',
+      './src/assemble/patterns/patterns.less'
+    ])
+    .pipe(less({
+      compress: true
+    }))
+    .pipe(prefix())
+    .pipe(concat('bundle.css'))
+    .pipe(gulp.dest('./dist/assets'))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
+});
